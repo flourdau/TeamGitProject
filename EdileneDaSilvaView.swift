36f77251263011ea8e1fa89f3a96bbe87a394dba
Edilene <edileneass@gmail.com>
2025-10-21 16:19:49 +0200

    Fichier SwiftUI ajouté

---
+++ EdileneDaSilvaView.swift
@@ -0,0 +1,21 @@
+//
+//  Untitled.swift
+//  EdileneDaSilvaView
+//
+//  Created by apprenant76 on 21/10/2025.
+//
+
+import SwiftUI
+
+struct EdileneDaSilvaView: View {
+    var body: some View {
+        VStack {
+            Text("Edilene Assunçao da Silva")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    EdileneDaSilvaView()
+}
