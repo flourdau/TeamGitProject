85f3b06337e7e61b096779dd68fafcc5e68c1412
Edilene <edileneass@gmail.com>
2025-10-21 16:46:50 +0200

    Added file

---
+++ TeamGitProject/EdileneView.swift
@@ -0,0 +1,18 @@
+//
+//  EdileneView.swift
+//  TeamGitProject
+//
+//  Created by apprenant76 on 21/10/2025.
+//
+
+import SwiftUI
+
+struct EdileneView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EdileneView()
+}
