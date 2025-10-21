3fade45f2ac71974464d79dc5bd0cbcf019e83ea
severineabt <severine.aubut@hotmail.fr>
2025-10-21 15:40:23 +0200

    first commit

---
+++ TeamGitProject/SeverineView.swift
@@ -0,0 +1,18 @@
+//
+//  SeverineView.swift
+//  TeamGitProject
+//
+//  Created by Apprenant 100 on 21/10/2025.
+//
+
+import SwiftUI
+
+struct SeverineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SeverineView()
+}
