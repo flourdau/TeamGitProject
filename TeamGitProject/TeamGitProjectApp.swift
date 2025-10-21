d8ee584fc9551ea4d6932adf827b7c9086bd79b5
Flourdau <lourdaux.florian.pro@gmail.com>
2025-10-21 14:51:07 +0200

    First commit, add xcode project

---
+++ TeamGitProject/TeamGitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamGitProjectApp.swift
+//  TeamGitProject
+//
+//  Created by apprenant78 on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TeamGitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
