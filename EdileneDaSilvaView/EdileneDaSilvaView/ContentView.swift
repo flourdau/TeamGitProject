5acae681808cbf724eed6c6b1701f5451aedca71
Edilene <edileneass@gmail.com>
2025-10-21 15:58:21 +0200

    Edilene's name

---
+++ EdileneDaSilvaView/EdileneDaSilvaView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EdileneDaSilvaView
+//
+//  Created by apprenant76 on 21/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Edilene Assunçao da Silva")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EdileneDaSilvaView/EdileneDaSilvaView/EdileneDaSilvaViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdileneDaSilvaViewApp.swift
+//  EdileneDaSilvaView
+//
+//  Created by apprenant76 on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct EdileneDaSilvaViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
